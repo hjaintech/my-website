95709a11724b7973a5be1f805823ce23a14c9f7a
Harshit Jain <hjain.tech@gmail.com>
2022-09-02 19:29:53 +0530

    update rl

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,7 +8,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'My Site',
   tagline: 'Dinosaurs are cool',
-  url: 'https://your-docusaurus-test-site.com',
+  url: 'https://hjaintech.github.io/',
   baseUrl: '/my-website/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
