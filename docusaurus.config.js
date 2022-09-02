29324724d51dac16ce08e309cb48895cf1e6a501
Harshit Jain <hjain.tech@gmail.com>
2022-09-02 19:25:16 +0530

    config change

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -16,9 +16,9 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
-
+  organizationName: 'hjaintech', // Usually your GitHub org/user name.
+  projectName: 'my-website', // Usually your repo name.
+  trailingSlash: false,
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
   // to replace "en" with "zh-Hans".
